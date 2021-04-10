5971e446f608b4cf4113f8984985ff48198d6ebc
AntonioPavkovic <antoniopavkovic999@gmail.com>
2021-04-10 11:12:58 +0200

    project in the workings

--- airlinedatabase/src/main/java/com/airlinedatabase/airlinedatabase/reposetory/PassangerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.airlinedatabase.airlinedatabase.reposetory;
-
-import com.airlinedatabase.airlinedatabase.model.Passanger;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PassangerRepository extends JpaRepository<Passanger, Long> {
-}
